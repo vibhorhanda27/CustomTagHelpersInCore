6fb0b7fbe632329fa133ee84751bc6c4d96e55aa
vibhor handa <vibhor.handa129@gmail.com>
2022-12-03 15:54:47 +0530

    Add project files.

---
+++ CustomHelpers/TagHelpers/AspActiveLinkTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace CustomHelpers
+{
+    [HtmlTargetElement("a",Attributes ="asp-active-link")]
+    public class AspactivelinkTagHelper: TagHelper
+    {
+
+        public string CssClassName { get; set; }
+        //private const string ActiveCssClass = "active";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            output.Attributes.SetAttribute("class",CssClassName);
+        }
+    }
+}
---
+++ CustomHelpers/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CustomHelpers
+{
+    [HtmlTargetElement("commandbutton")]
+    public class ButtonTagHelper:TagHelper
+    {
+        public string Type { get; set; } = "Submit";
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName= "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("type", Type);
+            output.Content.SetContent("Custom Command Button");
+        }
+    }
+}
